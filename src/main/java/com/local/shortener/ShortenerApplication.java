368eddb63639aeb383787904ef5268a2ba2cb771
Raymond Chua <kahhowchua.dev@gmail.com>
2021-10-14 00:23:12 +0800

    Disable Spring auto configuration for mongodb

--- src/main/java/com/local/shortener/ShortenerApplication.java
+++ src/main/java/com/local/shortener/ShortenerApplication.java
@@ -4,10 +4,12 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class ShortenerApplication {
 	
 	public static void main(String[] args) {
@@ -15,7 +17,7 @@ public class ShortenerApplication {
 	}
 	
 	public MongoClient mongoClient() {
-		return MongoClients.create("mongodb+srv://root:root@urlshortenercluster0.3lxio.mongodb.net/test?authSource=admin&replicaSet=atlas-1wjy41-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true\n");
+		return MongoClients.create("mongodb+srv://root:root@urlshortenercluster0.3lxio.mongodb.net/test?authSource=admin&replicaSet=atlas-1wjy41-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true");
 	}
 	
 	public @Bean MongoTemplate mongoTemplate() {
