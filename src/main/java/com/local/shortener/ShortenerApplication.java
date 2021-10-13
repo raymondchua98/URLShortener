2451297bed86e4079c49c730be3225f10b714eff
Raymond Chua <kahhowchua.dev@gmail.com>
2021-10-14 00:02:50 +0800

    Change connection from local to remote database

--- src/main/java/com/local/shortener/ShortenerApplication.java
+++ src/main/java/com/local/shortener/ShortenerApplication.java
@@ -1,12 +1,24 @@
 package com.local.shortener;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class ShortenerApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ShortenerApplication.class, args);
+	}
+	
+	public MongoClient mongoClient() {
+		return MongoClients.create("mongodb+srv://root:root@urlshortenercluster0.3lxio.mongodb.net/test?authSource=admin&replicaSet=atlas-1wjy41-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true\n");
+	}
+	
+	public @Bean MongoTemplate mongoTemplate() {
+		return new MongoTemplate(mongoClient(), "url-shortener");
 	}
 }
